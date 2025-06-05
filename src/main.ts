051a61f047aec4152041dff6eaaf1221175f4fa7
Eriel John Quilban Espinosa <ejqespinosa@addunet.ph>
2025-06-05 14:46:30 +0800

    feat: initial commit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT ?? 3001
+
   await app.listen(3000);
+  
+  console.log('RUNNING ON PORT : ${PORT} ')
 }
-bootstrap();
+
+
+void bootstrap();
