ac9a7f9b179d3639f28f89673ebde48cf55bc163
Eriel John Quilban Espinosa <ejqespinosa@addunet.ph>
2025-06-09 15:27:34 +0800

    feat: database creation

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const PORT = process.env.PORT ?? 3001
 
   await app.listen(3000);
   
-  console.log('RUNNING ON PORT : ${PORT} ')
+  console.log('RUNNING ON PORT : ', PORT)
+  console.log('hello world!')
 }
 
 
